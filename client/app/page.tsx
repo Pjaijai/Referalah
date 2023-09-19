451c6cc7a53e6f97137924b9e125fee661b865cc
Pjaijai <paul6a24@hotmail.com>
2023-09-19 12:42:31 -0400

    Fix text

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -15,7 +15,7 @@ export default function IndexPage() {
       <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
         <div className="flex flex-col justify-center">
           <h1 className="text-3xl  md:text-4xl">
-            海外港人搵工，互相支持，互相幫助。
+            海外港人搵工搵Referral。
           </h1>
           <p className="max-w-[700px] text-lg text-muted-foreground">
             希望呢個平台幫到大家！祝大家一切順利！
