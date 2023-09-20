ac90bcf47d54ebb6881003a034f4c9a36844251c
Pjaijai <paul6a24@hotmail.com>
2023-09-19 23:21:31 -0400

    fix

--- client/components/google-analytics.tsx
+++ client/components/google-analytics.tsx
@@ -1,8 +1,10 @@
 "use client"
 
 import { useEffect } from "react"
-import { usePathname, useSearchParams } from "next/navigation"
+import { usePathname } from "next/navigation"
 import Script from "next/script"
+
+declare const window: any
 
 export default function GoogleAnalytics() {
   const pathname = usePathname()
