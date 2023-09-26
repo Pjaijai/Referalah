4f5dfa3cbc7b69e359623a6383da71528c80352b
Pjaijai <paul6a24@hotmail.com>
2023-09-26 13:47:12 -0400

    Add get user profile hook

--- client/hooks/api/user/useUpdateUserProfile.tsx
+++ client/hooks/api/user/useUpdateUserProfile.tsx
@@ -1,6 +1,6 @@
 import apiService from "@/utils/common/api"
 import { supabase } from "@/utils/services/supabase/config"
-import { useMutation } from "@tanstack/react-query"
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
 
 const useUpdateUserProfile = () => {
   return useMutation({
