4a17478eaaae7c2e19167966bf7ffdb8fc85d581
Pjaijai <paul6a24@hotmail.com>
2023-09-26 18:36:29 -0400

    Add refetch fpr get user profile

--- client/hooks/api/user/useGetUserprofile.tsx
+++ client/hooks/api/user/useGetUserprofile.tsx
@@ -5,7 +5,9 @@ const useGetUserprofile = (userUuid: string | null) => {
   return useQuery({
     queryKey: [`user-profile`, { userUuid }],
     queryFn: apiService.getUserProfile,
-    refetchOnWindowFocus: false,
+    refetchOnWindowFocus: true,
+    refetchOnMount: true,
+    refetchOnReconnect: true,
   })
 }
 
