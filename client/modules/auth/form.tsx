ed10d4dd756828dca9da96418d61764c8d2e0e6a
Pjaijai <paul6a24@hotmail.com>
2023-09-28 15:29:10 -0400

    Use high lighted link

--- client/modules/auth/form.tsx
+++ client/modules/auth/form.tsx
@@ -1,17 +1,16 @@
 "use client"
 
 import React from "react"
+import Link from "next/link"
 import { authFormSchema } from "@/modules/auth/validation"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import { z } from "zod"
 
-import { siteConfig } from "@/config/site"
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert"
 import { Button } from "@/components/ui/button"
 import { Form } from "@/components/ui/form"
 import FormTextInput from "@/components/customized-ui/form/input"
-import HighlightedLink from "@/components/customized-ui/links/highlighted"
 
 interface IAuthFormProps {
   onSubmit: any
@@ -44,25 +43,18 @@ const AuthForm: React.FunctionComponent<IAuthFormProps> = ({ onSubmit }) => {
         </Alert>
         <p className="text-muted-foreground">
           點擊「登入/註冊」按鈕即表示你同意
-          <HighlightedLink href={siteConfig.page.privacyPolicy.href}>
+          <Link
+            href={"/privacy"}
+            className="border-green-700 dark:border-yellow-300 text-green-700
+] dark:text-yellow-300 "
+          >
             私隱政策
-          </HighlightedLink>
-          及
-          <HighlightedLink href={siteConfig.page.termsAndConditions.href}>
-            服務條款
-          </HighlightedLink>
+          </Link>
           。<br />
           By clicking the "Sign in/Sign up" button, you agree to the{" "}
-          <HighlightedLink href={siteConfig.page.privacyPolicy.href}>
-            privacy policy
-          </HighlightedLink>{" "}
-          <span className="m-2">and</span>{" "}
-          <HighlightedLink href={siteConfig.page.termsAndConditions.href}>
-            Terms and Conditions
-          </HighlightedLink>
-          .
+          <Link href={"/privacy"}>privacy policy</Link>.
         </p>
-        <Button type="submit"> 登入/註冊 | Sign in/Sign up </Button>
+        <Button type="submit"> 登入/註冊 </Button>
       </form>
     </Form>
   )
