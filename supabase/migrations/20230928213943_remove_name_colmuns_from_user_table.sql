e1ea2f58c8c11f2c0e62a0e9250aa181a3972677
Pjaijai <paul6a24@hotmail.com>
2023-09-28 17:41:04 -0400

    Add migration script to drop name columns in user table

---
+++ supabase/migrations/20230928213943_remove_name_colmuns_from_user_table.sql
@@ -0,0 +1,10 @@
+
+alter table "public"."user" drop column "chinese_first_name";
+
+alter table "public"."user" drop column "chinese_last_name";
+
+alter table "public"."user" drop column "english_first_name";
+
+alter table "public"."user" drop column "english_last_name";
+
+
