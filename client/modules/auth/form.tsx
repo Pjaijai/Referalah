eb0718fd41b74413715b941c0434b393f288d584
Pjaijai <paul6a24@hotmail.com>
2023-09-28 15:29:10 -0400

    Use high lighted link

--- client/modules/auth/form.tsx
+++ client/modules/auth/form.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import React from "react"
+import Link from "next/link"
 import { authFormSchema } from "@/modules/auth/validation"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
@@ -10,7 +11,6 @@ import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert"
 import { Button } from "@/components/ui/button"
 import { Form } from "@/components/ui/form"
 import FormTextInput from "@/components/customized-ui/form/input"
-import Link from "next/link";
 
 interface IAuthFormProps {
   onSubmit: any
@@ -41,8 +41,18 @@ const AuthForm: React.FunctionComponent<IAuthFormProps> = ({ onSubmit }) => {
             你個電郵Email只會係你主動聯絡人個時先話俾對方知。
           </AlertDescription>
         </Alert>
-        <p className="text-muted-foreground">點擊「登入/註冊」按鈕即表示你同意<Link href={"/privacy"}>私隱政策</Link>。<br/>
-          By clicking the "Sign in/Sign up" button, you agree to the <Link href={"/privacy"}>privacy policy</Link>.
+        <p className="text-muted-foreground">
+          點擊「登入/註冊」按鈕即表示你同意
+          <Link
+            href={"/privacy"}
+            className="border-green-700 dark:border-yellow-300 text-green-700
+] dark:text-yellow-300 "
+          >
+            私隱政策
+          </Link>
+          。<br />
+          By clicking the "Sign in/Sign up" button, you agree to the{" "}
+          <Link href={"/privacy"}>privacy policy</Link>.
         </p>
         <Button type="submit"> 登入/註冊 </Button>
       </form>
