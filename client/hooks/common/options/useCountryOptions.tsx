0ac46d67a54a9849587a81bb101c95f57c50d068
Pjaijai <paul6a24@hotmail.com>
2023-09-28 21:55:20 -0400

    Update on use option hooks

---
+++ client/hooks/common/options/useCountryOptions.tsx
@@ -0,0 +1,20 @@
+import { useMemo } from "react"
+
+import { ICountryResponse } from "@/types/api/response/country"
+
+const useCountryOptions = (countryList?: ICountryResponse[]) => {
+  return useMemo(
+    () =>
+      countryList
+        ? countryList.map((country) => {
+            return {
+              value: country.uuid,
+              title: `${country.english_name} | ${country.cantonese_name}`,
+            }
+          })
+        : [],
+    [countryList]
+  )
+}
+
+export default useCountryOptions
---
+++ client/hooks/common/options/useIndustryOptions.tsx
@@ -0,0 +1,21 @@
+import React, { useMemo } from "react"
+
+import { IIndustryResponse } from "@/types/api/response/industry"
+
+const useIndustryOptions = (industryList?: IIndustryResponse[]) => {
+  const industryOptions = useMemo(
+    () =>
+      industryList
+        ? industryList.map((industry) => {
+            return {
+              value: industry.uuid,
+              title: `${industry.english_name} | ${industry.cantonese_name}`,
+            }
+          })
+        : [],
+    [industryList]
+  )
+  return industryOptions
+}
+
+export default useIndustryOptions
