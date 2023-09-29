dcc1ff46abacb2f2d790bd21ca80288c84bf64b5
Pjaijai <paul6a24@hotmail.com>
2023-09-28 23:21:36 -0400

    Fix private policy route

--- client/components/customized-ui/footer/nav.tsx
+++ client/components/customized-ui/footer/nav.tsx
@@ -1,39 +1,19 @@
 import React from "react"
 import Link from "next/link"
 
-import { siteConfig } from "@/config/site"
 import { Icons } from "@/components/icons"
 
 const NavFooter = () => {
   return (
-    <footer className="mt-12 flex flex-col items-center justify-end gap-2 border-t-2 p-2 md:flex-row">
-      <div className="flex flex-row items-center gap-1 text-sm text-muted-foreground ">
-        <a href="mailto:r1r69.referalah@gmail.com">
-          <Icons.mail />
-        </a>
-
-        <Link
-          className="text-sm text-muted-foreground"
-          href={siteConfig.links.github}
-          target="_blank"
-          rel="noreferrer"
-        >
-          <Icons.github />
-        </Link>
-
-        <Link
-          className="text-sm text-muted-foreground"
-          href={siteConfig.page.privacyPolicy.href}
-        >
-          Privacy Policy
-        </Link>
-        <Link
-          className="text-sm text-muted-foreground"
-          href={siteConfig.page.termsAndConditions.href}
-        >
-          Terms and Conditions
-        </Link>
-      </div>
+    <footer className="border-t-2 mt-12 p-2 flex flex-row items-center gap-2">
+      <p className="text-muted-foreground text-sm">Created By R1R69</p>
+      <p className="flex flex-row text-muted-foreground text-sm items-center gap-1">
+        <Icons.mail />
+        <span>r1r69.referalah@gmail.com</span>
+      </p>
+      <Link className="text-muted-foreground text-sm" href={"/privacy-policy"}>
+        Privacy Policy
+      </Link>
     </footer>
   )
 }
