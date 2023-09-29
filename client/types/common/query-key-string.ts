ad171d631db26b56f57b474e9d6c3e8bc4de0de5
Pjaijai <paul6a24@hotmail.com>
2023-09-28 21:55:47 -0400

    Add query-key-string enum

--- client/types/common/query-key-string.ts
+++ client/types/common/query-key-string.ts
@@ -1,14 +1,6 @@
-enum QueryKeyString {
+export enum QueryKeyString {
   INDUSTRY_LIST = "industry-list",
   CITY_LIST = "city-list",
   COUNTRY_LIST = "country-list",
   PROVINCE_LIST = "province-list",
-  USER_PROFILE = "user-profile",
-  COUNT_USER = "count-user",
-  SEARCH_REFERRER_POST = "search-referrer-post",
-  SEARCH_REFEREE_POST = "search-referee-post",
-  SEARCH_REFERRER = "search-referrer",
-  SEARCH_REFEREE = "search-referee",
 }
-
-export { QueryKeyString }
