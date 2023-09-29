bd9fec8cbc8e2da3b2d16ae34e04afddc47fa63a
Pjaijai <paul6a24@hotmail.com>
2023-09-28 23:26:25 -0400

    Remove main nav

--- client/components/site-header.tsx
+++ client/components/site-header.tsx
@@ -2,13 +2,9 @@
 
 import UserDropDownMenu from "@/components/customized-ui/drop-down-menu/user"
 import { BaseNavigationMenu } from "@/components/customized-ui/navigation-menu/base"
-import { Icons } from "@/components/icons"
-import { MainNav } from "@/components/main-nav"
 import { ThemeToggle } from "@/components/theme-toggle"
 import { Button } from "@/components/ui/button"
-import useUserStore from "@/hooks/state/user/store"
-import { siteConfig } from "@/config/site"
-import Link from "next/link"
+import useUserStore from "@/hooks/state/user/useUserStore"
 import { useRouter } from "next/navigation"
 
 export function SiteHeader() {
@@ -18,7 +14,6 @@ export function SiteHeader() {
   return (
     <header className="bg-background sticky top-0 z-40 w-full border-b">
       <div className="container flex h-16 items-center space-x-4 justify-between sm:space-x-0">
-      <MainNav />
         <div className="hidden md:flex justify-center">
           <BaseNavigationMenu />
         </div>
@@ -26,14 +21,6 @@ export function SiteHeader() {
         <div className="flex  items-center justify-end space-x-4">
           <nav className="flex items-center space-x-1">
             <ThemeToggle />
-            <Link
-              href={siteConfig.links.github}
-              target="_blank"
-              rel="noreferrer"
-            >
-            <Icons.github />
-            </Link>
-
             {isUserSignIn ? (
               <UserDropDownMenu />
             ) : (
