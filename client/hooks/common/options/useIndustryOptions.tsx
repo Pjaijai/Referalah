e657fb2fbaeae0386ff76a4e827e79a9a6e5bb38
Pjaijai <paul6a24@hotmail.com>
2023-09-28 21:55:20 -0400

    Update on use option hooks

--- client/hooks/common/options/useIndustryOptions.tsx
+++ client/hooks/common/options/useIndustryOptions.tsx
@@ -2,15 +2,17 @@ import React, { useMemo } from "react"
 
 import { IIndustryResponse } from "@/types/api/response/industry"
 
-const useIndustryOptions = (industryList: IIndustryResponse[]) => {
+const useIndustryOptions = (industryList?: IIndustryResponse[]) => {
   const industryOptions = useMemo(
     () =>
-      industryList.map((industry) => {
-        return {
-          value: industry.uuid,
-          title: `${industry.english_name} | ${industry.cantonese_name}`,
-        }
-      }),
+      industryList
+        ? industryList.map((industry) => {
+            return {
+              value: industry.uuid,
+              title: `${industry.english_name} | ${industry.cantonese_name}`,
+            }
+          })
+        : [],
     [industryList]
   )
   return industryOptions
