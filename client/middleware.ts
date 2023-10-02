c726e9c51dcd9a746a6897bd50600615bf1834e8
Pjaijai <paul6a24@hotmail.com>
2023-10-02 10:08:46 -0400

    Refactor supabase auth

---
+++ client/middleware.ts
@@ -0,0 +1,9 @@
+import { NextResponse, type NextRequest } from "next/server"
+import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs"
+
+export async function middleware(req: NextRequest) {
+  const res = NextResponse.next()
+  const supabase = createMiddlewareClient({ req, res })
+  await supabase.auth.getSession()
+  return res
+}
