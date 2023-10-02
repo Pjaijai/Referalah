f702ab9f53e47c741cfa63e59405aad3c5381ee5
Thomas Yau <yaucp@connect.hku.hk>
2023-10-02 15:01:01 +0800

    update footer tnc link & fix eslint

--- client/components/customized-ui/footer/nav.tsx
+++ client/components/customized-ui/footer/nav.tsx
@@ -6,22 +6,22 @@ import { Icons } from "@/components/icons"
 
 const NavFooter = () => {
   return (
-    <footer className="border-t-2 mt-12 p-2 flex flex-col md:flex-row items-center gap-2 justify-between">
-      <p className="text-muted-foreground text-sm">Created By R1R69</p>
-      <p className="flex flex-row text-muted-foreground text-sm items-center gap-1">
+    <footer className="mt-12 flex flex-col items-center justify-between gap-2 border-t-2 p-2 md:flex-row">
+      <p className="text-sm text-muted-foreground">Created By R1R69</p>
+      <p className="flex flex-row items-center gap-1 text-sm text-muted-foreground">
         <Icons.mail />
         <span>r1r69.referalah@gmail.com</span>
       </p>
       <div className="flex gap-2">
         <Link
-          className="text-muted-foreground text-sm"
+          className="text-sm text-muted-foreground"
           href={siteConfig.page.privacyPolicy.href}
         >
           Privacy Policy
         </Link>
         <Link
-          className="text-muted-foreground text-sm"
-          href={siteConfig.page.privacyPolicy.href}
+          className="text-sm text-muted-foreground"
+          href={siteConfig.page.termsAndConditions.href}
         >
           Terms and Conditions
         </Link>
