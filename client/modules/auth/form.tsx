627bee1f329e46fadcee9211c9855ff81f3bfcce
Pjaijai <87293994+Pjaijai@users.noreply.github.com>
2023-10-02 00:53:01 -0400

    Feat/add tnc (#64)
    * Add t and c page
    
    * Create highlighted link
    
    * Add TNC
    
    ---------
    
    Co-authored-by: Pjaijai <paul6a24@hotmail.com>

--- client/modules/auth/form.tsx
+++ client/modules/auth/form.tsx
@@ -1,7 +1,6 @@
 "use client"
 
 import React from "react"
-import Link from "next/link"
 import { authFormSchema } from "@/modules/auth/validation"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
