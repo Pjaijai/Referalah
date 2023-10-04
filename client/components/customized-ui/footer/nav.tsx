3b6aaf70531910bece760fcdb71a4e31dc6d86fe
Pjaijai <87293994+Pjaijai@users.noreply.github.com>
2023-10-04 07:12:41 -0400

    Feature/add GitHub link (#79)
    * Adding github link
    
    * Fix engine
    
    ---------
    
    Co-authored-by: Pjaijai <paul6a24@hotmail.com>

--- client/components/customized-ui/footer/nav.tsx
+++ client/components/customized-ui/footer/nav.tsx
@@ -6,13 +6,21 @@ import { Icons } from "@/components/icons"
 
 const NavFooter = () => {
   return (
-    <footer className="mt-12 flex flex-col items-center justify-between gap-2 border-t-2 p-2 md:flex-row">
-      <p className="text-sm text-muted-foreground">Created By R1R69</p>
-      <p className="flex flex-row items-center gap-1 text-sm text-muted-foreground">
-        <Icons.mail />
-        <span>r1r69.referalah@gmail.com</span>
-      </p>
-      <div className="flex gap-2">
+    <footer className="mt-12 flex flex-col items-center justify-end gap-2 border-t-2 p-2 md:flex-row">
+      <div className="flex flex-row items-center gap-1 text-sm text-muted-foreground ">
+        <a href="mailto:r1r69.referalah@gmail.com">
+          <Icons.mail />
+        </a>
+
+        <Link
+          className="text-sm text-muted-foreground"
+          href={siteConfig.links.github}
+          target="_blank"
+          rel="noreferrer"
+        >
+          <Icons.github />
+        </Link>
+
         <Link
           className="text-sm text-muted-foreground"
           href={siteConfig.page.privacyPolicy.href}
