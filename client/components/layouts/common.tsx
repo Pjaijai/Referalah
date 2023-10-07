7d030717e7ea988c7df89fc0cc40c0db5626626b
Pjaijai <paul6a24@hotmail.com>
2023-10-07 10:45:21 -0400

    Rename page history layout to common

--- client/components/layouts/common.tsx
+++ client/components/layouts/common.tsx
@@ -1,17 +1,17 @@
 import React, { PropsWithChildren } from "react"
 
-interface SearchPageLayout {
+interface ICommonPageLayout {
   title: string
 }
-const SearchPageLayout: React.FunctionComponent<
-  PropsWithChildren<SearchPageLayout>
+const CommonPageLayout: React.FunctionComponent<
+  PropsWithChildren<ICommonPageLayout>
 > = ({ title, children }) => {
   return (
-    <div className="w-full h-ull">
+    <div className="w-full h-full">
       <h1 className="text-center font-bold mt-4 text-2xl">{title}</h1>
       {children}
     </div>
   )
 }
 
-export default SearchPageLayout
+export default CommonPageLayout
