3b0eedbbffc4c7ebf16d3d02b1815217e4fe6644
Thomas Yau <yaucp@connect.hku.hk>
2023-10-09 01:33:02 +0800

    add CORS to edge func depending on the staging env

--- supabase/functions/_shared/cors.ts
+++ supabase/functions/_shared/cors.ts
@@ -1,5 +1,12 @@
+import "https://deno.land/std@0.177.0/dotenv/load.ts";
+
+enum stagingEnvironment {
+  PRODUCTION = "PRODUCTION",
+  STAGING = "STAGING",
+}
+
 export const corsHeaders = {
-  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Origin": Deno.env.get("ENVIRONMENT") === stagingEnvironment.PRODUCTION ? Deno.env.get("WEB_BASE_URL") : "*" ,
   "Access-Control-Allow-Headers":
-    "authorization, x-client-info, apikey, content-type",
+      "authorization, x-client-info, apikey, content-type",
 }
