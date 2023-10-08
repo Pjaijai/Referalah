ad7d1b25dc48470e650916bcdbfb0a3591cb9db9
Thomas Yau <yaucp@connect.hku.hk>
2023-10-09 00:53:53 +0800

    update cors origin to depend on staging env

--- supabase/functions/_shared/cors.ts
+++ supabase/functions/_shared/cors.ts
@@ -1,5 +1,8 @@
+import stagingEnvironments from "./stagingEnvironments.ts";
+import "https://deno.land/std@0.177.0/dotenv/load.ts";
+
 export const corsHeaders = {
-  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Origin": Deno.env.get("ENVIRONMENT") === stagingEnvironments.PRODUCTION ? Deno.env.get("WEB_BASE_URL") : "*" ,
   "Access-Control-Allow-Headers":
     "authorization, x-client-info, apikey, content-type",
 }
