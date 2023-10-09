49a478abd4b4ed0ea961995e705ba3145f2736d4
Pjaijai <paul6a24@hotmail.com>
2023-10-08 21:07:14 -0400

    Add doc to query key string

--- client/types/common/query-key-string.ts
+++ client/types/common/query-key-string.ts
@@ -1,12 +1,22 @@
 enum QueryKeyString {
+  // List
   INDUSTRY_LIST = "industry-list",
   CITY_LIST = "city-list",
   COUNTRY_LIST = "country-list",
   PROVINCE_LIST = "province-list",
+
+  // Profile
   USER_PROFILE = "user-profile",
+
+  // Statistic
   COUNT_USER = "count-user",
+
+  // Post
   SEARCH_REFERRER_POST = "search-referrer-post",
   SEARCH_REFEREE_POST = "search-referee-post",
+  GET_POSTS_BY_USER_UUID = "get_posts_by_user_uuid",
+
+  // Referral
   SEARCH_REFERRER = "search-referrer",
   SEARCH_REFEREE = "search-referee",
 }
