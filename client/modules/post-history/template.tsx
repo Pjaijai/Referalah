8e03722855520a8c684ef012b10568fe482a4cbe
Pjaijai <paul6a24@hotmail.com>
2023-10-08 21:09:05 -0400

    Create post history page template

--- client/modules/post-history/template.tsx
+++ client/modules/post-history/template.tsx
@@ -1,7 +1,41 @@
+"use client"
+
 import React from "react"
+import PostHistoryCard from "@/modules/post-history/components/cards/post-history"
+
+import useGetPostsByUserUuid from "@/hooks/api/post/get-by-userUuid"
+import useUserStore from "@/hooks/state/user/store"
 
 const PostHistoryTemplate = () => {
-  return <div>PostHistoryTemplate</div>
+  const userUuid = useUserStore((state) => state.uuid)
+  const { data: posts } = useGetPostsByUserUuid(userUuid)
+
+  return (
+    <div className="w-full h-full">
+      <div className="flex flex-col gap-4 mt-8">
+        {posts &&
+          posts.map((data) => (
+            <PostHistoryCard
+              city={data.city.cantonese_name}
+              companyName={data.company_name}
+              country={data.country.cantonese_name}
+              createdAt={data.created_at}
+              description={data.description}
+              id={data.id}
+              industry={data.industry.cantonese_name}
+              jobTitle={data.job_title}
+              province={data.province.cantonese_name}
+              status={data.status}
+              type={data.type}
+              url={data.url}
+              uuid={data.uuid}
+              yearOfExperience={data.year_of_experience}
+              key={data.uuid}
+            />
+          ))}
+      </div>
+    </div>
+  )
 }
 
 export default PostHistoryTemplate
