f5ac3a4090d3f1373879022ce53d611fb7bed6a3
Pjaijai <87293994+Pjaijai@users.noreply.github.com>
2023-10-15 20:54:55 -0400

    Feature/adding type from supabase (#124)
    * Adding supabase type
    
    * Adding workflow files
    
    * fix types
    
    ---------
    
    Co-authored-by: Pjaijai <paul6a24@hotmail.com>

--- client/types/api/response/province.ts
+++ client/types/api/response/province.ts
@@ -1,8 +1,8 @@
 export interface IProvinceResponse {
+  cantonese_name: string | null
+  country_uuid: string | null
+  english_name: string | null
   id: number
   uuid: string
   value: string
-  country_uuid: string
-  english_name: string
-  cantonese_name: string
 }
