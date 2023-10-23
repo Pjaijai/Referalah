8b91f12915372e8c5a15c3524dd805bea579e2cc
Pjaijai <paul6a24@hotmail.com>
2023-10-22 21:15:20 -0400

    refactor common layout

--- client/components/layouts/common.tsx
+++ client/components/layouts/common.tsx
@@ -1,14 +1,15 @@
 import React, { PropsWithChildren } from "react"
 
 interface CommonPageLayout {
-  title: string
+  title?: string
 }
 const CommonPageLayout: React.FunctionComponent<
   PropsWithChildren<CommonPageLayout>
 > = ({ title, children }) => {
   return (
-    <div className="h-full w-full">
-      <h1 className="mt-4 text-center text-2xl font-bold">{title}</h1>
+    <div className="mt-4 h-full w-full">
+      {title && <h1 className=" text-center text-2xl font-bold">{title}</h1>}
+
       {children}
     </div>
   )
