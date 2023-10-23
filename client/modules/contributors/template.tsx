10038becbcd730994381676389a145b1364d7d1c
Pjaijai <paul6a24@hotmail.com>
2023-10-22 22:55:24 -0400

    Update contribution template

--- client/modules/contributors/template.tsx
+++ client/modules/contributors/template.tsx
@@ -1,5 +1,28 @@
+import ContributorCard from "@/app/contributors/components/contributor-card"
+import { IContributor } from "@/app/contributors/types/contributor"
+
+const contributors: IContributor[] = [
+  {
+    name: "Paul Wong (aka R1R69)",
+    links: {
+      github: "https://github.com/Pjaijai",
+      linkedin: "https://www.linkedin.com/in/paulwong169/",
+    },
+    contributedArea: ["software_development", "uiux_design"],
+  },
+]
 const ContributorsPageTemplate = () => {
-  return <div>ContributorsPageTemplate</div>
+  return (
+    <>
+      {contributors.map((contributor) => (
+        <ContributorCard
+          contributedArea={contributor.contributedArea}
+          links={contributor.links}
+          name={contributor.name}
+        />
+      ))}
+    </>
+  )
 }
 
 export default ContributorsPageTemplate
