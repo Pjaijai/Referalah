c8e6e2535e00a065e7c4edac141579008ab8fc73
Pjaijai <paul6a24@hotmail.com>
2023-10-22 22:53:18 -0400

    Create getContributionAreaText

---
+++ client/app/contributors/uilts/get-contribution-area-text.ts
@@ -0,0 +1,14 @@
+import { ContributedArea } from "@/app/contributors/types/ contributed-area"
+
+const getContributionAreaText = (area: ContributedArea) => {
+  const mapper: Record<ContributedArea, string> = {
+    software_development: "軟件開發",
+    uiux_design: "UX/UI設計",
+  }
+
+  const text = mapper[area]
+
+  return text
+}
+
+export default getContributionAreaText
