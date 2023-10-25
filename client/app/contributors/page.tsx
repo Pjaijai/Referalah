bb182a6bac7d0222200622eb635af2a32d04a0b9
Pjaijai <paul6a24@hotmail.com>
2023-10-24 21:08:34 -0400

    Adding common page layout to contributors page

--- client/app/contributors/page.tsx
+++ client/app/contributors/page.tsx
@@ -1,8 +1,15 @@
 import React from "react"
 import ContributorsPageTemplate from "@/modules/contributors/template"
 
+import { siteConfig } from "@/config/site"
+import CommonPageLayout from "@/components/layouts/common"
+
 const ContributorsPage = () => {
-  return <ContributorsPageTemplate />
+  return (
+    <CommonPageLayout title={siteConfig.page.contributors.name}>
+      <ContributorsPageTemplate />
+    </CommonPageLayout>
+  )
 }
 
 export default ContributorsPage
