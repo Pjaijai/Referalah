5e2bee8e8721c819c438cb4d1d7e8f2bbcfe5a40
Pjaijai <paul6a24@hotmail.com>
2023-10-24 21:09:17 -0400

    Fix footer css

--- client/components/customized-ui/footer/nav.tsx
+++ client/components/customized-ui/footer/nav.tsx
@@ -8,7 +8,7 @@ import { Icons } from "@/components/icons"
 
 const NavFooter = () => {
   return (
-    <footer className="mt-12 flex flex-col items-center justify-center gap-2 border-t-2 p-2 md:flex-row">
+    <footer className="mt-12 flex flex-col items-center justify-center gap-2 border-t-2 p-2 md:flex-row ">
       <div className="flex flex-col items-center gap-1 text-sm text-muted-foreground ">
         <div className="flex flex-row gap-2">
           <a href="mailto:r1r69.referalah@gmail.com">
@@ -25,30 +25,30 @@ const NavFooter = () => {
           </Link>
         </div>
 
-        <div className="flex gap-2">
+        <div className="flex flex-col gap-2 md:flex-row">
           <Link
-            className="text-sm text-muted-foreground"
+            className="text-center text-sm text-muted-foreground"
             href={siteConfig.page.privacyPolicy.href}
           >
             Privacy Policy
           </Link>
-          |
+
           <Link
-            className="text-sm text-muted-foreground"
+            className="text-center text-sm text-muted-foreground"
             href={siteConfig.page.termsAndConditions.href}
           >
             Terms and Conditions
           </Link>
-          |
+
           <Link
-            className="text-sm text-muted-foreground"
+            className="text-center text-sm text-muted-foreground"
             href={siteConfig.page.about.href}
           >
             {siteConfig.page.about.name}
           </Link>
-          |
+
           <Link
-            className="text-sm text-muted-foreground"
+            className="text-center text-sm text-muted-foreground"
             href={siteConfig.page.contributors.href}
           >
             {siteConfig.page.contributors.name}
