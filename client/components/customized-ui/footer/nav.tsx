8cd74f0a1a1f6f09fbff5603c616d3f6e6e00fe6
Pjaijai <paul6a24@hotmail.com>
2023-10-24 20:43:32 -0400

    update footer

--- client/components/customized-ui/footer/nav.tsx
+++ client/components/customized-ui/footer/nav.tsx
@@ -1,13 +1,16 @@
+"use client"
+
 import React from "react"
 import Link from "next/link"
 
 import { siteConfig } from "@/config/site"
+import { Separator } from "@/components/ui/separator"
 import { Icons } from "@/components/icons"
 
 const NavFooter = () => {
   return (
-    <footer className="mt-12 flex flex-col items-center justify-end gap-2 border-t-2 p-2 md:flex-row">
-      <div className="flex flex-row items-center gap-1 text-sm text-muted-foreground ">
+    <footer className="mt-12 flex flex-col items-center justify-center gap-2 border-t-2 p-2 md:flex-row">
+      <div className="flex flex-col items-center gap-1 text-sm text-muted-foreground ">
         <a href="mailto:r1r69.referalah@gmail.com">
           <Icons.mail />
         </a>
@@ -21,18 +24,27 @@ const NavFooter = () => {
           <Icons.github />
         </Link>
 
-        <Link
-          className="text-sm text-muted-foreground"
-          href={siteConfig.page.privacyPolicy.href}
-        >
-          Privacy Policy
-        </Link>
-        <Link
-          className="text-sm text-muted-foreground"
-          href={siteConfig.page.termsAndConditions.href}
-        >
-          Terms and Conditions
-        </Link>
+        <div className="flex flex-row gap-2">
+          <Link
+            className="text-sm text-muted-foreground"
+            href={siteConfig.page.privacyPolicy.href}
+          >
+            Privacy Policy
+          </Link>
+          <Separator orientation="vertical" />
+          <Link
+            className="text-sm text-muted-foreground"
+            href={siteConfig.page.termsAndConditions.href}
+          >
+            Terms and Conditions
+          </Link>
+          <Link
+            className="text-sm text-muted-foreground"
+            href={siteConfig.page.about.href}
+          >
+            {siteConfig.page.about.name}
+          </Link>
+        </div>
       </div>
     </footer>
   )
