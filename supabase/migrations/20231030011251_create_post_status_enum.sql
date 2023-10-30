8d5a1cf533887fe6dab77ed19bae29475b5e7d71
Pjaijai <paul6a24@hotmail.com>
2023-10-29 21:45:00 -0400

    Remove useless line

--- supabase/migrations/20231030011251_create_post_status_enum.sql
+++ supabase/migrations/20231030011251_create_post_status_enum.sql
@@ -6,6 +6,3 @@ alter table "public"."post" alter column "status" set not null;
 
 alter table "public"."post" alter column "status" set data type post_status using "status"::post_status;
 
-set check_function_bodies = off;
-
-
