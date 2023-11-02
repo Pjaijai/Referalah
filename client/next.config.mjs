8f8820f998da5f66a078a4623d9f2471b60f56e7
Eddie Li <14104647ls3@gmail.com>
2023-11-01 21:21:06 -0400

    Feat: Added PWA configuration

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,12 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+
+import withPWA from 'next-pwa';
+
+const pwaConfig = withPWA({
+  dest: "public",
+}
+)
+export default pwaConfig({
   reactStrictMode: true,
   experimental: {
     appDir: true,
   },
   images: {
     domains: ["localhost"], // Add the domain where your images are hosted
-  },
-}
-
-export default nextConfig
+  }
+});
