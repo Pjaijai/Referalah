ce3796833e59e2e6f6a49718aba087a7d6b089df
Pjaijai <paul6a24@hotmail.com>
2023-11-01 21:44:58 -0400

    Revert "Bug: These 2 files exist for no reason"
    This reverts commit b0b1fe0f7296a74cc36cacea18d0972e098fd937.

---
+++ client/components/customized-ui/skeletons /card-list.tsx
@@ -0,0 +1,29 @@
+import React from "react"
+
+import { cn } from "@/lib/utils"
+import CardSkeleton, {
+  CardSkeletonProps,
+} from "@/components/customized-ui/skeletons /card"
+
+interface CardSkeletonListProps {
+  className?: string
+  cardSkeletonProps?: CardSkeletonProps
+}
+
+const CardSkeletonList = ({
+  className,
+  cardSkeletonProps,
+}: CardSkeletonListProps) => {
+  return (
+    <div className={cn("mt-8 grid grid-cols-1 gap-4", className)}>
+      <CardSkeleton {...cardSkeletonProps} />
+      <CardSkeleton {...cardSkeletonProps} />
+      <CardSkeleton {...cardSkeletonProps} />
+      <CardSkeleton {...cardSkeletonProps} />
+      <CardSkeleton {...cardSkeletonProps} />
+      <CardSkeleton {...cardSkeletonProps} />
+    </div>
+  )
+}
+
+export default CardSkeletonList
