64953a3263cc3839c8cfb40fd4056f9d536d0d45
Pjaijai <paul6a24@hotmail.com>
2023-11-02 23:27:17 -0400

    create edit page

---
+++ client/app/post/edit/[postUuid]/page.tsx
@@ -0,0 +1,19 @@
+"use client"
+
+import React from "react"
+import EditPostPageTemplate from "@/modules/post/edit/template"
+
+import useGetPost from "@/hooks/api/post/get-post"
+import CommonPageLayout from "@/components/layouts/common"
+
+const EditPostPage = ({ params }: { params: { postUuid: string } }) => {
+  const { data: post, isLoading, isSuccess } = useGetPost(params.postUuid)
+
+  return (
+    <CommonPageLayout>
+      <EditPostPageTemplate postDate={post} isLoading={isLoading} />
+    </CommonPageLayout>
+  )
+}
+
+export default EditPostPage
