dd8f5299a4db1122fbe614b75a962451e11e6636
Pjaijai <paul6a24@hotmail.com>
2023-11-03 14:36:50 -0400

    Update edit post validation schema

--- client/modules/post/validation/edit.ts
+++ client/modules/post/validation/edit.ts
@@ -1,6 +1,12 @@
 import { createPostValidationSchema } from "@/modules/post/validation/create"
 import { z } from "zod"
 
-const editPostValidationSchema = z.object({}).merge(createPostValidationSchema)
+import { PostStatus } from "@/types/common/post-status"
+
+const editPostValidationSchema = z
+  .object({
+    status: z.enum([PostStatus.ACTIVE, PostStatus.INACTIVE]),
+  })
+  .merge(createPostValidationSchema)
 
 export { editPostValidationSchema }
