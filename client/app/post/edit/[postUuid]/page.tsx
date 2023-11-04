7b495b79d9d5b04b74d51f0e4575bea435efbae5
Pjaijai <paul6a24@hotmail.com>
2023-11-03 23:00:56 -0400

    Add user return hook

--- client/app/post/edit/[postUuid]/page.tsx
+++ client/app/post/edit/[postUuid]/page.tsx
@@ -1,18 +1,34 @@
 "use client"
 
-import React from "react"
+import React, { useEffect } from "react"
+import { useRouter } from "next/navigation"
 import EditPostPageTemplate from "@/modules/post/edit/template"
 
+import { siteConfig } from "@/config/site"
 import useGetPost from "@/hooks/api/post/get-post"
+import useUserStore from "@/hooks/state/user/store"
 import CommonPageLayout from "@/components/layouts/common"
 
 const EditPostPage = ({ params }: { params: { postUuid: string } }) => {
-  const { data: post, isLoading, isSuccess } = useGetPost(params.postUuid)
+  const { data: post, isLoading } = useGetPost(params.postUuid)
+  const userUuid = useUserStore((state) => state.uuid)
+  const router = useRouter()
+
+  // If not sign in and not viewing by same user
+  useEffect(() => {
+    if (!isLoading && post?.user?.uuid !== userUuid) {
+      router.push(siteConfig.page.main.href)
+    }
+  }, [isLoading, post, router, userUuid])
 
   return (
     <CommonPageLayout>
       {!isLoading && (
-        <EditPostPageTemplate postDate={post} isPostDataLoading={isLoading} />
+        <EditPostPageTemplate
+          postDate={post}
+          isPostDataLoading={isLoading}
+          postUuid={params.postUuid}
+        />
       )}
     </CommonPageLayout>
   )
