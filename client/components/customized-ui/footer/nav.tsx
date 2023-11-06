a3c0e7a890f8e9ae8ff7c24ffd67027bdd498129
Lulu Tung <lulutheflaneur@gmail.com>
2023-11-06 11:16:57 -0500

    Add spacing to clickable elements to enhance SEO

--- client/components/customized-ui/footer/nav.tsx
+++ client/components/customized-ui/footer/nav.tsx
@@ -9,8 +9,8 @@ import { Icons } from "@/components/icons"
 const NavFooter = () => {
   return (
     <footer className="mt-12 flex flex-col items-center justify-center gap-2 border-t-2 p-2 md:flex-row ">
-      <div className="flex flex-col items-center gap-1 text-sm text-muted-foreground ">
-        <div className="flex flex-row gap-2">
+      <div className="flex flex-col items-center gap-3 text-sm text-muted-foreground ">
+        <div className="flex flex-row gap-3">
           <a href="mailto:r1r69.referalah@gmail.com">
             <Icons.mail />
           </a>
@@ -25,7 +25,7 @@ const NavFooter = () => {
           </Link>
         </div>
 
-        <div className="flex flex-col gap-2 md:flex-row">
+        <div className="flex flex-col gap-3 md:flex-row">
           <Link
             className="text-center text-sm text-muted-foreground"
             href={siteConfig.page.privacyPolicy.href}
