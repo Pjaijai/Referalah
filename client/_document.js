ba54e68340a565aba4d73a6bbc2c3aa5dd6056b0
Pjaijai <paul6a24@hotmail.com>
2023-11-05 22:33:09 -0500

    Update _document

--- client/_document.js
+++ client/_document.js
@@ -1,4 +1,4 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
+import Document, { Head, Html, Main, NextScript } from "next/document"
 
 class MyDocument extends Document {
   render() {
@@ -6,16 +6,16 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="manifest" href="/manifest.json" />
-          <link rel="apple-touch-icon" href="/icon.png"></link>
-          <meta name="theme-color" content="#fff" />
+          <link rel="referalah-icon" href="/icon.png"></link>
+          <meta name="theme-color" content="##ffffff" />
         </Head>
         <body>
           <Main />
           <NextScript />
         </body>
       </Html>
-    );
+    )
   }
 }
 
-export default MyDocument;
+export default MyDocument
