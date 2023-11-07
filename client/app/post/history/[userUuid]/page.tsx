289d2baa8803ab20fe48e42a5079cb4f0fc37fd8
Pjaijai <87293994+Pjaijai@users.noreply.github.com>
2023-11-07 11:31:21 -0500

    Feature/post history page button in profile page (#215)
    * Adding site config
    
    * Adding post history page title
    
    * House keep
    
    * Adding history icon
    
    * Adding post history button to profile page
    
    * Adding metadata
    
    * House keep
    
    * Fix seo metadata generation problem
    
    ---------
    
    Co-authored-by: Pjaijai <paul6a24@hotmail.com>

--- client/app/post/history/[userUuid]/page.tsx
+++ client/app/post/history/[userUuid]/page.tsx
@@ -1,24 +1,33 @@
-"use client"
-
 import React from "react"
 import PostHistoryTemplate from "@/modules/post/history/template"
+import { getUserProfile } from "@/utils/common/api"
 
-import useGetPostListByUserUuid from "@/hooks/api/post/get-post-list-by-user-uuid"
-import useUserStore from "@/hooks/state/user/store"
+import { siteConfig } from "@/config/site"
 import CommonPageLayout from "@/components/layouts/common"
 
-const PostHistoryPage = ({ params }: { params: { userUuid: string } }) => {
-  const { data, isLoading } = useGetPostListByUserUuid(params.userUuid)
-  const userUuid = useUserStore((state) => state.uuid)
-  const isViewingOwnProfile = params.userUuid === userUuid
+export async function generateMetadata({
+  params,
+}: {
+  params: { userUuid: string }
+}) {
+  const { userUuid } = params
+  try {
+    const res = await getUserProfile({ queryKey: [, { userUuid }] })
+    return {
+      title: res.username + "街招記錄",
+    }
+  } catch (e) {
+    return {
+      title: "街招記錄",
+      description: "街招記錄",
+    }
+  }
+}
 
+const PostHistoryPage = ({ params }: { params: { userUuid: string } }) => {
   return (
-    <CommonPageLayout>
-      <PostHistoryTemplate
-        data={data}
-        isLoading={isLoading}
-        isViewingOwnProfile={isViewingOwnProfile}
-      />
+    <CommonPageLayout title={siteConfig.page.postHistory.name}>
+      <PostHistoryTemplate slug={params.userUuid} />
     </CommonPageLayout>
   )
 }
