99ec09894cb157b5aa8fb3e78a11c2d41f43fabd
Pjaijai <paul6a24@hotmail.com>
2023-11-06 23:07:47 -0500

    Adding metadata

--- client/app/post/history/[userUuid]/page.tsx
+++ client/app/post/history/[userUuid]/page.tsx
@@ -2,11 +2,31 @@
 
 import React from "react"
 import PostHistoryTemplate from "@/modules/post/history/template"
+import { getUserProfile } from "@/utils/common/api"
 
 import { siteConfig } from "@/config/site"
 import useGetPostListByUserUuid from "@/hooks/api/post/get-post-list-by-user-uuid"
 import useUserStore from "@/hooks/state/user/store"
 import CommonPageLayout from "@/components/layouts/common"
+
+export async function generateMetadata({
+  params,
+}: {
+  params: { userUuid: string }
+}) {
+  const { userUuid } = params
+  try {
+    const res = await getUserProfile({ queryKey: [, { userUuid }] })
+    return {
+      title: res.username + "街招記錄",
+    }
+  } catch (e) {
+    return {
+      title: "街招記錄",
+      description: "街招記錄",
+    }
+  }
+}
 
 const PostHistoryPage = ({ params }: { params: { userUuid: string } }) => {
   const { data, isLoading } = useGetPostListByUserUuid(params.userUuid)
