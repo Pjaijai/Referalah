1a3723d5fa185006aa785906a8bb5c09633833d1
Pjaijai <paul6a24@hotmail.com>
2023-11-07 13:06:55 -0500

    Refactor api

--- client/hooks/api/user/get-user-profile.tsx
+++ client/hooks/api/user/get-user-profile.tsx
@@ -3,10 +3,15 @@ import { useQuery } from "@tanstack/react-query"
 
 import { QueryKeyString } from "@/types/common/query-key-string"
 
+const getProfile = ({ queryKey }: any) => {
+  const userUuid = queryKey[1].userUuid
+  return getUserProfile(userUuid)
+}
+
 const useGetUserprofile = (userUuid: string | null) => {
   return useQuery({
     queryKey: [QueryKeyString.USER_PROFILE, { userUuid }],
-    queryFn: getUserProfile,
+    queryFn: getProfile,
     refetchOnWindowFocus: true,
     refetchOnMount: true,
     refetchOnReconnect: true,
