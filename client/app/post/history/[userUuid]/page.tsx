30d8df88253d69983898c276b7d2794923a10a7c
Pjaijai <paul6a24@hotmail.com>
2023-11-06 23:16:22 -0500

    Fix seo metadata generation problem

--- client/app/post/history/[userUuid]/page.tsx
+++ client/app/post/history/[userUuid]/page.tsx
@@ -1,12 +1,8 @@
-"use client"
-
 import React from "react"
 import PostHistoryTemplate from "@/modules/post/history/template"
 import { getUserProfile } from "@/utils/common/api"
 
 import { siteConfig } from "@/config/site"
-import useGetPostListByUserUuid from "@/hooks/api/post/get-post-list-by-user-uuid"
-import useUserStore from "@/hooks/state/user/store"
 import CommonPageLayout from "@/components/layouts/common"
 
 export async function generateMetadata({
@@ -29,17 +25,9 @@ export async function generateMetadata({
 }
 
 const PostHistoryPage = ({ params }: { params: { userUuid: string } }) => {
-  const { data, isLoading } = useGetPostListByUserUuid(params.userUuid)
-  const userUuid = useUserStore((state) => state.uuid)
-  const isViewingOwnProfile = params.userUuid === userUuid
-
   return (
     <CommonPageLayout title={siteConfig.page.postHistory.name}>
-      <PostHistoryTemplate
-        data={data}
-        isLoading={isLoading}
-        isViewingOwnProfile={isViewingOwnProfile}
-      />
+      <PostHistoryTemplate slug={params.userUuid} />
     </CommonPageLayout>
   )
 }
