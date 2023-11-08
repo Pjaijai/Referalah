4714a58e53c294ce2b34a0b109505b04d5363364
Paco Wong <86609598+paco0161@users.noreply.github.com>
2023-11-08 03:07:41 -0500

    Feature/error boundary (#218)
    * Add root error boundary
    
    * add myself as contributor

---
+++ client/app/error.tsx
@@ -0,0 +1,23 @@
+'use client'
+
+import { Button } from "@/components/ui/button"
+
+interface GlobalErrorProps {
+	error: Error & { digest?: string }
+  reset: () => void
+}
+
+const GlobalError = ({
+  error,
+  reset,
+}: GlobalErrorProps) => {
+  return (
+    <div className="flex h-[500px] flex-col items-center justify-center gap-4">
+      <span className="text-5xl">🥲😢</span>
+      <h6>{error.message || "Something went wrong"}</h6>
+      <Button type="button" onClick={reset}>Try again</Button>
+    </div>
+  )
+}
+
+export default GlobalError
