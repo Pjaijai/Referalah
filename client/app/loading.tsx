a30036b8e37f266e2144327d3266f154e8be95ea
Ken Tung <tungpakyin04@outlook.com>
2023-11-07 19:38:32 -0800

    update to use spinner

--- client/app/loading.tsx
+++ client/app/loading.tsx
@@ -1,7 +1,9 @@
+import { Icons } from "@/components/icons"
+
 export default function Loading() {
-  return <div
-    className="h-screen flex justify-center items-center text-xl"
-   >
-    Loading...
-  </div>
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <Icons.loader className="animate-spin text-2xl" />
+    </div>
+  )
 }
