5ca3c2906ce6e568000c668a63354351bbbbf62c
Pjaijai <paul6a24@hotmail.com>
2023-11-19 10:36:14 -0500

    Adding config

---
+++ client/jest.config.mjs
@@ -0,0 +1,30 @@
+// /** @type {import('ts-jest').JestConfigWithTsJest} */
+// module.exports = {
+//   preset: "ts-jest",
+//   testEnvironment: "node",
+//   moduleNameMapper: {
+//     "@/(.*)": "<rootDir>/$1",
+//   },
+//   collectCoverageFrom: ["**/*.{ts,tsx}", "!**/node_modules/**"],
+//   runner: "groups",
+// }
+
+// jest.config.mjs
+import nextJest from "next/jest.js"
+
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: "./",
+})
+
+// Add any custom config to be passed to Jest
+/** @type {import('jest').Config} */
+const config = {
+  // Add more setup options before each test is run
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  runner: "groups",
+  testEnvironment: "jest-environment-jsdom",
+}
+
+// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+export default createJestConfig(config)
