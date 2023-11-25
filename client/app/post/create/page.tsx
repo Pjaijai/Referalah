87dcb4a327665bc11e4713827ef4725a21162f1d
Pjaijai <paul6a24@hotmail.com>
2023-11-25 00:07:38 -0500

    chore: house keep

--- client/app/post/create/page.tsx
+++ client/app/post/create/page.tsx
@@ -1,7 +1,6 @@
 import CreatePostTemplate from "@/modules/post/create/template"
 
 import { siteConfig } from "@/config/site"
-import AuthenticatedPageLayout from "@/components/layouts/authenticated"
 import CommonPageLayout from "@/components/layouts/common"
 import AuthenticatedPageWrapper from "@/components/wrappers/authenticated"
 
