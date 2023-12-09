4afd35156c4c7769f827e75837d88b5eb149c9d5
Pjaijai <paul6a24@hotmail.com>
2023-12-08 20:27:46 -0500

    fix: fix validation text

--- client/modules/auth/validations/password.ts
+++ client/modules/auth/validations/password.ts
@@ -2,5 +2,5 @@ import { z } from "zod"
 
 export const authPasswordValidationSchema = z
   .string()
-  .min(6, { message: "too short" })
-  .max(20, { message: "too long" })
+  .min(6, { message: "最少6粒字" })
+  .max(20, { message: "最多20粒字" })
