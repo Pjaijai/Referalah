2cfaecd7f3fe9ddee80292a2274fda79dfcdb7b5
Pjaijai <paul6a24@hotmail.com>
2023-12-09 23:37:12 -0500

    style: fixing margin

--- client/modules/auth/components/tabs/sign-in.tsx
+++ client/modules/auth/components/tabs/sign-in.tsx
@@ -6,7 +6,7 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 
 const SignInTab = () => {
   return (
-    <Tabs defaultValue="password" className="mt-4 h-72 w-full md:w-[400px]">
+    <Tabs defaultValue="password" className="mt-8 h-72 w-full md:w-[400px]">
       <TabsList className="w-full">
         <TabsTrigger value="password" className="w-full">
           密碼
--- client/modules/auth/sign-up/template.tsx
+++ client/modules/auth/sign-up/template.tsx
@@ -4,7 +4,7 @@ import SignUpForm from "@/modules/auth/components/forms/sign-up"
 const SignUpPageTemplate = () => {
   return (
     <div className="flex h-full w-full justify-center">
-      <div className="h-full w-full max-w-[400px]">
+      <div className="mt-8 h-full w-full max-w-[400px]">
         <SignUpForm />
       </div>
     </div>
