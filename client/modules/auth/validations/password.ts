15a560da26d0d4a3b032f6e9be8ff4d71f42e11f
Pjaijai <paul6a24@hotmail.com>
2023-12-13 01:09:15 -0500

    fix: fix password min length validation

--- client/modules/auth/validations/password.ts
+++ client/modules/auth/validations/password.ts
@@ -2,5 +2,5 @@ import { z } from "zod"
 
 export const authPasswordValidationSchema = z
   .string()
-  .min(6, { message: "最少6粒字" })
+  .min(8, { message: "最少8粒字" })
   .max(20, { message: "最多20粒字" })
