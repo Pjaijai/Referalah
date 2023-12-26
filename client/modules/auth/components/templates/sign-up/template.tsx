87eabe1c58486b14dbeb2695ae3c03a540d59267
Pjaijai <paul6a24@hotmail.com>
2023-12-25 22:35:41 -0500

    chore: replace throw with replace

--- client/modules/auth/components/templates/sign-up/template.tsx
+++ client/modules/auth/components/templates/sign-up/template.tsx
@@ -9,7 +9,7 @@ import useUserStore from "@/hooks/state/user/store"
 const SignUpPageTemplate = () => {
   const isUserSignIn = useUserStore((state) => state.isSignIn)
 
-  if (isUserSignIn) throw notFound()
+  if (isUserSignIn) return notFound()
   return (
     <div className="flex h-full w-full justify-center">
       <div className="mt-8 h-full w-full max-w-[400px]">
