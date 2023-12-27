c96a93005453c8abab4cfb0963d172a4f0ee0881
Pjaijai <paul6a24@hotmail.com>
2023-12-27 14:22:00 -0500

    style: more style on room card

--- client/modules/chat/components/sections/left/left.tsx
+++ client/modules/chat/components/sections/left/left.tsx
@@ -3,7 +3,7 @@ import ChatRoomList from "@/modules/chat/components/lists/room/room"
 
 const ChatLeftSection = () => {
   return (
-    <div className="w-[35%]">
+    <div className="w-[35%] p-2">
       <ChatRoomList />
     </div>
   )
