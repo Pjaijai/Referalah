8d731225ba34f3422e2d2d67fdf304daed7e19b2
Pjaijai <paul6a24@hotmail.com>
2023-12-27 21:36:44 -0500

    chore: house keep

--- supabase/functions/_shared/client.ts
+++ supabase/functions/_shared/client.ts
@@ -2,10 +2,13 @@ import { createClient } from "https://esm.sh/@supabase/supabase-js@2"
 
 const initSupabaseClient = (req: any) => {
   return createClient(
+    // Supabase API URL - env var exported by default.
     Deno.env.get("SUPABASE_URL") ?? "",
-
-    Deno.env.get("SUPABASE_ANON_KEY") ?? "",
-
+    // Supabase API ANON KEY - env var exported by default.
+    // Deno.env.get("SUPABASE_ANON_KEY") ?? "",
+    Deno.env.get("SUPABASE_SERVICE_ROLE_KEY") ?? "",
+    // Create client with Auth context of the user that called the function.
+    // This way your row-level-security (RLS) policies are applied.
     {
       auth: { persistSession: false },
       global: { headers: { Authorization: req.headers.get("Authorization")! } },
