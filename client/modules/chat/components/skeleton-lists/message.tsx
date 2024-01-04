ae468f64514e84e7692a23da2014979e4c554d38
Pjaijai <paul6a24@hotmail.com>
2024-01-04 15:50:11 -0500

    style: chat message list skletion style

--- client/modules/chat/components/skeleton-lists/message.tsx
+++ client/modules/chat/components/skeleton-lists/message.tsx
@@ -1,12 +1,18 @@
 import React from "react"
 
+import { cn } from "@/lib/utils"
 import CardSkeleton from "@/components/customized-ui/skeletons/card"
 
 const MessageListSkeleton = () => {
-  const numberOfCards = 10
+  const numberOfCards = 15
   const skeletonCards = Array.from({ length: numberOfCards }, (_, index) => (
-    <div className="w-full">
-      <CardSkeleton key={index} className="w-20 rounded-sm md:h-20" />
+    <div
+      className={cn(
+        "flex w-full flex-row ",
+        index % 3 === 0 ? "justify-start" : "justify-end"
+      )}
+    >
+      <CardSkeleton key={index} className="w-28 rounded-lg md:h-12 " />
     </div>
   ))
   return <div className="flex h-full flex-col gap-2">{skeletonCards}</div>
