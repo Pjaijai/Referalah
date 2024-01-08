4c26f94c99c6b8a242d71de733eed6f534a27343
Pjaijai <paul6a24@hotmail.com>
2024-01-07 21:25:08 -0500

    fix: fix build error

--- client/components/icons.tsx
+++ client/components/icons.tsx
@@ -42,7 +42,7 @@ export type Icon = typeof LucideIcon
 
 export const Icons = {
   arrowLeft: ArrowLeft,
-
+  messageSquareMore: MessageSquareMore,
   sun: SunMedium,
   moon: Moon,
   twitter: Twitter,
@@ -50,7 +50,6 @@ export const Icons = {
   link: Link,
   laptop: Laptop,
   mail: Mail,
-  messageSquareMore: MessageSquareMore,
   pencil: Pencil,
   loader: Loader,
   undo: Undo2,
@@ -66,6 +65,24 @@ export const Icons = {
   history: History,
   eye: Eye,
   eyeOff: EyeOff,
+  smallArrowLeft: (props: LucideProps) => (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="16"
+      height="16"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      className="lucide lucide-arrow-left"
+      {...props}
+    >
+      <path d="m12 19-7-7 7-7" />
+      <path d="M19 12H5" />
+    </svg>
+  ),
   sendHorizontal: SendHorizontal,
   send: Send,
   dot: Dot,
