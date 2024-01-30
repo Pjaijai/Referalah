e3a13bb95431265ebec29fec78d60243e0234902
Pjaijai <paul6a24@hotmail.com>
2024-01-29 21:06:06 -0500

    fix: fix sign up redirect problem

--- client/app/[locale]/auth/sign-in/page.tsx
+++ client/app/[locale]/auth/sign-in/page.tsx
@@ -4,15 +4,18 @@ import { getI18n } from "@/utils/services/internationalization/server"
 
 import { siteConfig } from "@/config/site"
 import CommonPageLayout from "@/components/layouts/common"
+import UnauthenticatedPageWrapper from "@/components/wrappers/unauthenticated"
 
 export const metadata = siteConfig.page.signIn.metadata
 
 const SignInPage = async () => {
   const t = await getI18n()
   return (
-    <CommonPageLayout title={t("page.sign_in")}>
-      <SignInPageTemplate />
-    </CommonPageLayout>
+    <UnauthenticatedPageWrapper>
+      <CommonPageLayout title={t("page.sign_in")}>
+        <SignInPageTemplate />
+      </CommonPageLayout>
+    </UnauthenticatedPageWrapper>
   )
 }
 export default SignInPage
