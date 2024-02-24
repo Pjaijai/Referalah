7871b45f2675640b78ed3c41604c8e49a4b7520e
Pjaijai <paul6a24@hotmail.com>
2024-02-23 22:18:39 -0500

    style: fix profile info card ui padding

--- client/modules/profile/components/cards/Info.tsx
+++ client/modules/profile/components/cards/Info.tsx
@@ -5,7 +5,7 @@ interface IInfoCardProps {
 }
 const InfoCard: React.FunctionComponent<IInfoCardProps> = ({ children }) => {
   return (
-    <div className="flex h-fit w-fit min-w-[10rem]  flex-col items-center justify-center gap-2 rounded-lg border border-slate-300 bg-slate-50 px-6 py-2 dark:border-muted-foreground dark:bg-black">
+    <div className="flex h-fit w-fit min-w-[8rem]  flex-col items-center justify-center gap-2 rounded-lg border border-slate-300 bg-slate-50 px-6 py-2 dark:border-muted-foreground dark:bg-black">
       {children}
     </div>
   )
