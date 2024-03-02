a919a54f6eb118ba1a5b9cf4557bdd5c6de2d28e
Pjaijai <87293994+Pjaijai@users.noreply.github.com>
2024-03-02 17:26:37 -0500

    feat: post preview on main page (#329)
    Co-authored-by: Pjaijai <paul6a24@hotmail.com>

--- client/types/api/request/post/search.ts
+++ client/types/api/request/post/search.ts
@@ -3,14 +3,14 @@ import { EReferralType } from "@/types/common/referral-type"
 export interface ISearchPostsRequest {
   numberOfDataPerPage: number
   type: EReferralType
-  countryUuid?: string
-  provinceUuid?: string
-  cityUuid?: string
-  industryUuid?: string
   companyName: string
   maxYearOfExperience: number
   minYearOfExperience: number
   jobTitle: string
   sortingType: string
   page: number
+  countryUuid?: string
+  provinceUuid?: string
+  cityUuid?: string
+  industryUuid?: string
 }
