cb48248e713625438d48394421fe3368ee26c14b
Pjaijai <87293994+Pjaijai@users.noreply.github.com>
2024-04-21 21:46:49 -0400

    caching (#353)
    Co-authored-by: Pjaijai <paul6a24@hotmail.com>

--- client/app/[locale]/referer/page.tsx
+++ client/app/[locale]/referer/page.tsx
@@ -1,4 +1,3 @@
-import { NextPage } from "next"
 import RefererPageTemplate from "@/modules/referral/referer/template"
 import {
   getCityList,
@@ -7,12 +6,14 @@ import {
   getProvinceList,
 } from "@/utils/common/api"
 import { getI18n } from "@/utils/services/internationalization/server"
-import { supabase } from "@/utils/services/supabase/config"
 
 import { siteConfig } from "@/config/site"
 import CommonPageLayout from "@/components/layouts/common"
 
 export const metadata = siteConfig.page.referrer.metadata
+
+export const revalidate = false
+export const fetchCache = "default-cache"
 
 export default async function RefererPage() {
   const t = await getI18n()
