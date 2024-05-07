baa71657dd0f2e1ba3562766cb5cfd0bb7820ba2
Pjaijai <paul6a24@hotmail.com>
2024-05-06 23:15:44 -0400

    chore: house keep highlighted link

--- client/components/customized-ui/links/highlighted.tsx
+++ client/components/customized-ui/links/highlighted.tsx
@@ -10,7 +10,7 @@ const HighlightedLink: React.FunctionComponent<
   return (
     <Link
       href={href}
-      className="] border-b border-green-700
+      className=" border-b border-green-700
 text-green-700 hover:border-b-2 dark:border-yellow-300 dark:text-yellow-300"
     >
       {children}
