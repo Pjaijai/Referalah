8778b6dd3082f2e332ee9d657c1e14607ce2a729
Pjaijai <paul6a24@hotmail.com>
2024-08-14 23:59:34 -0400

    feat: update referral post card

--- client/components/customized-ui/cards/referral-post.tsx
+++ client/components/customized-ui/cards/referral-post.tsx
@@ -1,20 +1,21 @@
 import React from "react"
 import Link from "next/link"
 import { useRouter } from "next/navigation"
-import PostCardInfoDisplay from "@/modules/post/components/info-display/card-info"
-import PostHeader from "@/modules/post/components/info-display/header"
 import usePostTypeTitle from "@/modules/post/hooks/post-type-title"
 
 import { EPostType } from "@/types/common/post-type"
 import { siteConfig } from "@/config/site"
 import { cn } from "@/lib/utils"
 import { Badge } from "@/components/ui/badge"
-import { Card, CardFooter, CardHeader } from "@/components/ui/card"
+import { Card, CardContent, CardFooter, CardHeader } from "@/components/ui/card"
 import { Separator } from "@/components/ui/separator"
 import BaseAvatar from "@/components/customized-ui/avatars/base"
 import ContactRequestCount from "@/components/customized-ui/icons/contact-request-count"
-import CompanyNameDisplay from "@/components/customized-ui/info-display/company"
 import CreatedAtDisplay from "@/components/customized-ui/info-display/created-at"
+import IndustryDisplay from "@/components/customized-ui/info-display/industry"
+import LocationDisplay from "@/components/customized-ui/info-display/location"
+import YearsOfExperienceDisplay from "@/components/customized-ui/info-display/years-of-experience"
+import { Icons } from "@/components/icons"
 
 interface IReferralPostCardProps {
   uuid: string | null
@@ -35,8 +36,6 @@ interface IReferralPostCardProps {
   requestCount: number
 }
 
-// NOTE: please use onClick with e.preventDefault() for any links inside this component to prevent validateDOMNesting warning
-
 const ReferralPostCard: React.FunctionComponent<IReferralPostCardProps> = ({
   type,
   uuid,
@@ -44,10 +43,10 @@ const ReferralPostCard: React.FunctionComponent<IReferralPostCardProps> = ({
   city,
   companyName,
   country,
-  industry,
   photoUrl,
   province,
   url,
+  industry,
   username,
   yearOfExperience,
   createdAt,
@@ -63,35 +62,79 @@ const ReferralPostCard: React.FunctionComponent<IReferralPostCardProps> = ({
   }
 
   const postTypeTitle = usePostTypeTitle(type)
+  const handleUrlClick = (e: any) => {
+    e.preventDefault()
+    if (url) window.open(url, "_blank")
+  }
 
   return (
     <Card
       className={cn(
-        "flex h-fit shrink-0 flex-col justify-between rounded shadow-md ",
+        "flex  max-w-sm shrink-0 flex-col justify-between rounded border-none shadow-lg",
         className
       )}
     >
       <Link
         href={`${siteConfig.page.viewPost.href}/${uuid}`}
         onClick={(e) => e.stopPropagation()}
-        className="flex h-full flex-col items-start justify-start"
       >
-        <div className="flex w-full flex-col items-start justify-start">
-          <CardHeader className="w-full pb-2">
-            {/* title, subtitle, url, avatar, quick action */}
-            <div className="flex flex-row items-start justify-between gap-3 sm:gap-1">
-              <div className="mb-2 flex basis-full items-center gap-3 sm:basis-2/3 md:basis-3/5">
-                <PostHeader
-                  title={jobTitle}
-                  subtitle={
-                    companyName ? (
-                      <CompanyNameDisplay name={companyName} />
-                    ) : undefined
-                  }
-                  url={url}
-                />
-              </div>
+        <div className="flex min-h-fit w-full shrink-0 flex-col items-start justify-start">
+          <CardHeader className="w-full">
+            <div className="flex w-full flex-col items-start justify-between rounded-lg bg-slate-50 p-4">
+              <h2 className="line-clamp-2 w-full text-xl font-semibold">
+                {jobTitle}
+              </h2>
+              <p className="w-full truncate text-sm font-medium">
+                {companyName}
+              </p>
+            </div>
+          </CardHeader>
+        </div>
 
+        <CardContent className="flex w-full flex-col">
+          <div className="mt-4 flex w-full justify-between">
+            <Badge className="w-fit rounded-xl bg-indigo-100 px-2 py-1 text-sm font-medium text-indigo-600 hover:bg-indigo-100">
+              {postTypeTitle}
+            </Badge>
+            <div className="flex flex-row items-center justify-center gap-4">
+              <ContactRequestCount count={requestCount} />
+
+              {url && (
+                <div onClick={handleUrlClick}>
+                  <Icons.link className="h-4 w-4 align-middle" />
+                </div>
+              )}
+            </div>
+          </div>
+
+          <div className="mt-8 flex flex-row items-end">
+            {/* location, industry, year of exp */}
+            <div className="flex w-full flex-col items-start justify-start gap-2 text-sm font-normal">
+              {(city || province || country) && (
+                <LocationDisplay
+                  city={city}
+                  province={province}
+                  country={country}
+                  className="gap-2"
+                />
+              )}
+
+              {industry && <IndustryDisplay industry={industry} />}
+              {typeof yearOfExperience === "number" && (
+                <YearsOfExperienceDisplay
+                  yearOfExperience={yearOfExperience}
+                  className="gap-2"
+                />
+              )}
+            </div>
+          </div>
+          <Separator className="mt-6" />
+        </CardContent>
+
+        {/* created at */}
+        <CardFooter className="mt-2 flex w-full flex-row  items-center justify-between">
+          <div className="mt-2 flex w-full flex-row justify-between">
+            <div className="flex flex-row items-center justify-center gap-4">
               <div onClick={handleAvatarOnClick}>
                 <BaseAvatar
                   fallBack={username ? username[0] : "?"}
@@ -99,29 +142,11 @@ const ReferralPostCard: React.FunctionComponent<IReferralPostCardProps> = ({
                   url={photoUrl || undefined}
                 />
               </div>
-            </div>
 
-            <div className="flex w-full flex-col items-start justify-start gap-1 md:flex-row md:items-end md:justify-between">
-              {/* location, industry, year of exp */}
-              <PostCardInfoDisplay
-                city={city}
-                province={province}
-                country={country}
-                industry={industry}
-                yearOfExperience={yearOfExperience}
-                requestCount={requestCount}
-              />
+              <p>{username}</p>
             </div>
-            <Separator />
-          </CardHeader>
-        </div>
-
-        {/* created at */}
-        <CardFooter className="mt-2 flex w-full flex-row justify-between">
-          {postTypeTitle && (
-            <Badge className="flex justify-center">{postTypeTitle}</Badge>
-          )}
-          <CreatedAtDisplay applyTo="card" createdAt={createdAt} />
+            <CreatedAtDisplay applyTo="card" createdAt={createdAt} />
+          </div>
         </CardFooter>
       </Link>
     </Card>
