5a5e9e02d2591db5c59ba44f58e851a1158e9f56
Pjaijai <paul6a24@hotmail.com>
2024-08-15 00:04:32 -0400

    style: add purple to base badge

--- client/components/ui/badge.tsx
+++ client/components/ui/badge.tsx
@@ -18,8 +18,7 @@ const badgeVariants = cva(
         success: "bg-green-300 border-green-300 text-black hover:bg-green-300",
         error: "bg-red-300 border-red-300 text-black hover:bg-red-300",
         blue: "bg-blue-100 border-blue-100 text-black hover:bg-blue-300",
-        purple:
-          "bg-purple-100 border-purple-100 text-black hover:bg-purple-100",
+        purple: "bg-purple-100 border-purple-100 text-black ",
       },
     },
     defaultVariants: {
