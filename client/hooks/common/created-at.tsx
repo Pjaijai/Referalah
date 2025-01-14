324cac88a53699027993c6b07dda0af6fc783b9c
Paul Wong <87293994+Pjaijai@users.noreply.github.com>
2025-01-13 21:24:15 -0500

    fix: mahe usrecreatedAt support display years (#410)
    Co-authored-by: Pjaijai <paul6a24@hotmail.com>

--- client/hooks/common/created-at.tsx
+++ client/hooks/common/created-at.tsx
@@ -1,31 +1,45 @@
 import { useMemo } from "react"
-import { formatDate } from "@/utils/common/helpers/format/date"
 import compareDateDifferenceHelper from "@/utils/common/helpers/time/compare-date-difference"
 
 const useCreatedAt = ({ createdAt }: { createdAt: string | null }) => {
   const formattedCreatedAt = useMemo(() => {
     if (!createdAt) return "- -"
+
+    const now = new Date().toISOString()
+    const yearDiff = compareDateDifferenceHelper({
+      oldDate: createdAt,
+      newDate: now,
+      unit: "year",
+    })
+
+    if (yearDiff > 0) return `${yearDiff}y`
+
     const dayDiff = compareDateDifferenceHelper({
       oldDate: createdAt,
-      newDate: new Date().toISOString(),
+      newDate: now,
       unit: "day",
     })
 
-    if (dayDiff > 7 && dayDiff <= 364) return `${Math.ceil(dayDiff / 7)}w`
+    if (dayDiff > 30) return `${Math.floor(dayDiff / 30)}mo`
+    if (dayDiff > 7) return `${Math.floor(dayDiff / 7)}w`
+    if (dayDiff > 0) return `${dayDiff}d`
 
-    if (dayDiff > 0 && dayDiff <= 7) return `${dayDiff}d`
+    const hourDiff = compareDateDifferenceHelper({
+      oldDate: createdAt,
+      newDate: now,
+      unit: "hour",
+    })
+
+    if (hourDiff > 0) return `${hourDiff}h`
 
     const minuteDiff = compareDateDifferenceHelper({
       oldDate: createdAt,
-      newDate: new Date().toISOString(),
+      newDate: now,
       unit: "minute",
     })
 
-    if (minuteDiff > 60 && minuteDiff <= 1440)
-      return `${Math.ceil(minuteDiff / 60)}h`
-    if (minuteDiff > 0 && minuteDiff <= 60) return `${minuteDiff}m`
-    if (minuteDiff === 0) return "now"
-    return formatDate("DD/MM/YY")
+    if (minuteDiff > 0) return `${minuteDiff}m`
+    return "now"
   }, [createdAt])
 
   return {
