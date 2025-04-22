cd2a809f26bfcf5fe73dc5265a3ec2d2b875cd64
Pjaijai <paul6a24@hotmail.com>
2025-04-22 00:04:47 -0400

    feat: api types

---
+++ client/types/api/company/index.ts
@@ -0,0 +1,14 @@
+export type TCompanyData = {
+  id: number
+  name: string
+  logo_url: string | null
+  created_at?: string
+}
+export type TCompanyResponse = TCompanyData
+
+export type TCompany = {
+  id: number
+  name: string
+  logoUrl: string | null
+  createdAt?: string
+}
