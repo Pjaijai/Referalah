f4978d83fff486cc6d9dc48770b964ffd1dd971a
Paul Wong <87293994+Pjaijai@users.noreply.github.com>
2025-05-02 15:40:19 -0400

    Feature/main page first content pain (#432)
    * feat: increase cache
    
    * style: fix main page user count style
    
    ---------
    
    Co-authored-by: Pjaijai <paul6a24@hotmail.com>

--- client/modules/main/components/user-count.tsx
+++ client/modules/main/components/user-count.tsx
@@ -12,9 +12,9 @@ const UserCount: React.FunctionComponent<IUserCountProps> = ({
   const scopedT = useScopedI18n("index")
 
   return (
-    <div className="mt-4 flex flex-row items-end gap-1">
-      <p className="text-xs font-medium">{scopedT("so_far")}</p>
-      <p className="text-[18px] font-black leading-none text-indigo-600 md:text-base ">
+    <div className="mt-4 flex flex-row items-center gap-1">
+      <p className="text-xs font-medium leading-none">{scopedT("so_far")}</p>
+      <p className="text-[18px] font-black leading-[1.125rem] text-indigo-600 md:text-base md:leading-4">
         {numberOfMembers || "0"}
       </p>
     </div>
