cb4163005cfdcaed966adc73c50afbe5a559c28a
Paul Wong <87293994+Pjaijai@users.noreply.github.com>
2026-01-07 16:11:08 -0500

    feat: remove member contract (#483)
    Co-authored-by: Pjaijai <paul6a24@hotmail.com>

--- client/modules/main/components/carousels/contact-request.tsx
+++ client/modules/main/components/carousels/contact-request.tsx
@@ -4,7 +4,6 @@ import React from "react"
 import { useScopedI18n } from "@/utils/services/internationalization/client"
 
 import {
-  IMemberContact,
   IPostContact,
   TContactRequestListResponse,
 } from "@/types/api/response/contact-request/contact-request-list"
@@ -28,20 +27,6 @@ const PostRow = (props: IPostContact) => {
   )
 }
 
-const MemberRow = (props: IMemberContact) => {
-  const { data: formattedCreatedAt } = useCreatedAt({
-    createdAt: props.createdAt,
-  })
-  const scopedT = useScopedI18n("index")
-  return (
-    <div className="flex flex-row gap-1 ">
-      <p>{formattedCreatedAt}</p>
-      <p className="ml-3">@{props.senderUserName} </p>
-      <p>{scopedT("contacted")}</p>
-      <p className="font-medium text-indigo-600">@{props.receiverUserName}</p>
-    </div>
-  )
-}
 interface IContactRequestCarouselProps {
   list: TContactRequestListResponse[]
 }
@@ -51,10 +36,9 @@ const ContactRequestCarousel: React.FunctionComponent<
   return (
     <div className="relative flex h-32 w-full flex-row items-center justify-center overflow-hidden  bg-slate-50 ">
       <Marquee vertical className="text-xs [--duration:60s]">
-        {list.map((data) => {
-          if (data.type === "post") return <PostRow {...data} />
-          if (data.type === "member") return <MemberRow {...data} />
-        })}
+        {list.map((data, index) => (
+          <PostRow key={index} {...data} />
+        ))}
       </Marquee>
       <div className="pointer-events-none absolute inset-x-0 top-0 h-1/3 bg-gradient-to-b from-slate-50 dark:from-background"></div>
       <div className="pointer-events-none absolute inset-x-0 bottom-0 h-1/3 bg-gradient-to-t from-slate-50 dark:from-background"></div>
